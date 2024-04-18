6381cb1ad45fae612f3d24b5958e986c6ce162e1
LaminarFlow24 <148671685+LaminarFlow24@users.noreply.github.com>
2024-04-18 19:09:01 +0530

    Add files via upload

---
+++ Exercise_Pose_Detector/app.py
@@ -0,0 +1,126 @@
+from flask import Flask, render_template, Response
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+cap = cv2.VideoCapture(0)
+
+app = Flask(__name__)
+
+
+
+def calculate_angle(a,b,c):
+    a = np.array(a) 
+    b = np.array(b) 
+    c = np.array(c) 
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle
+
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    
+    @app.route('/')
+    def index():
+        return render_template('Pullup.html')
+
+    def gen():
+        
+        
+        counter = 0 
+        stage = None
+
+        
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+        
+            
+            results = pose.process(image)
+        
+            
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )
+            
+            
+            try:
+                landmarks = results.pose_landmarks.landmark
+                
+                
+                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                
+                
+                angle = calculate_angle(shoulder, elbow, wrist)
+                
+                
+                cv2.putText(image, str(angle), 
+                            tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
+                
+                
+                if angle > 160:
+                    stage = "down"
+                if angle < 30 and stage =='down':
+                    stage="up"
+                    counter +=1
+                    print(counter)
+                        
+            except:
+                pass
+            
+            
+            cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+            
+            
+            cv2.putText(image, 'REPS', (15,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter), 
+                        (10,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            
+            cv2.putText(image, 'STAGE', (65,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage, 
+                        (60,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)            
+            
+            
+            
+            
+            ret, fin = cv2.imencode('.jpg',image)
+
+            din = fin.tobytes()
+
+
+            
+            yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + din + b'\r\n\r\n')    
+            
+    @app.route('/video_feed')
+    def video_feed():
+        return Response(gen(),
+                        mimetype='multipart/x-mixed-replace; boundary=frame') 
+
+    if __name__ == '__main__':
+        app.run(host = '0.0.0.0', port = '5000', debug= 'True')
